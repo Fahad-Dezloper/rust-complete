1f21db9cd746e68f901139474aa8957806749fcd
Fahad <fahad.khan2216@gmail.com>
2025-05-01 12:38:15 +0530

    starting

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-use chrono::{Utc, Local};
-use std::ops::Add;
-
-fn main(){
-    print!("{}", sum(1,2));
+fn main() {
+    println!("Hello, world!");
 }
-
-fn sum<T: Add<Output = T>>(a: T, b: T) -> T {
-    return a + b;
-}
-
-
-// let utc = Utc::now();
-// let local_time = Local::now();
-// print!("{}", utc);
-// print!("{}", local_time);
